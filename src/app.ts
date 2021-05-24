a8ab3893edfe99563ae7768365e2e4c6758f23da
Florian Näpflin <florian@3ap.ch>
2021-05-24 16:36:12 +0200

    implement endpoint for like / unlike movies

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import mongodb from 'mongodb';
-import cors from 'cors';
 import { DATABASE_COLLECTION_NAME, DATABASE_NAME, DATABASE_URL, SERVER_PORT } from "./config/config";
 
 const app: Application = express();
-app.use(cors);
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    next();
+});
 
 const MongoClient = mongodb.MongoClient;
 let db = undefined;
@@ -16,12 +20,70 @@ MongoClient.connect(DATABASE_URL, {useUnifiedTopology: true}, function (err, con
     collection = db.collection(DATABASE_COLLECTION_NAME);
 });
 
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).send({
-        message: "Hello World!",
+interface ResponseBody {
+    status: number;
+    payload: any;
+}
+
+const response = (status: number = 200, payload: any = null): ResponseBody => {
+    return {status: status, payload: payload};
+};
+
+app.post('/movie/:movieId/like', ((req, res) => {
+    const movieId: number = Number(req.params.movieId);
+    collection.find({movieId: movieId}).count().then((count: number) => {
+        if (count === 0) {
+            collection.insertOne({movieId: movieId}, error => {
+                if (error) {
+                    return res.send(response(500, error.toString()));
+                } else {
+                    return res.send(response(200));
+                }
+            });
+        } else {
+            return res.send(response(409));
+        }
+    });
+}));
+
+app.delete('/movie/:movieId/like', ((req, res) => {
+    const movieId: number = Number(req.params.movieId);
+    collection.find({movieId: movieId}).count().then((count: number) => {
+        if (count > 0) {
+            collection.deleteMany({movieId: movieId}, error => {
+                if (error) {
+                    return res.send(response(500, error.toString()));
+                } else {
+                    return res.send(response(200));
+                }
+            });
+        } else {
+            return res.send(response(409));
+        }
+    });
+}));
+
+app.get('/likes', (req, res) => {
+    collection.find({}).toArray(function (error, result) {
+        if (error) {
+            return res.send(response(500, error.toString()));
+        } else {
+            return res.send(response(200, result));
+        }
     });
 });
 
+app.get('/movie/:movieId', ((req, res) => {
+    const movieId: number = Number(req.params.movieId);
+    collection.find({movieId: movieId}).count().then((count: number) => {
+        if (count > 0) {
+            return res.send(response(204));
+        } else {
+            return res.send(response(404));
+        }
+    });
+}));
+
 app.listen(SERVER_PORT, () => {
-    console.log(`'server is running on port ${SERVER_PORT}.'`);
+    console.log(`'server is running on port ${SERVER_PORT}'`);
 });
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_PORT = 5000;
-export const DATABASE_URL = 'mongodb://localhost:27017';
-export const DATABASE_NAME = 'movieworld';
-export const DATABASE_COLLECTION_NAME = 'likes';
+export const SERVER_PORT: number = 5000;
+export const DATABASE_URL: string = 'mongodb://localhost:27017';
+export const DATABASE_NAME: string = 'movieworld';
+export const DATABASE_COLLECTION_NAME: string = 'likes';
